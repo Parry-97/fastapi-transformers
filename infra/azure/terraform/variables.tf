5d0757733d9eca1f40fdecde425cfa3fc1c2c118
Param Pal Singh <singhparampal97@gmail.com>
2025-07-19 21:15:16 +0200

    add type for terraform vars

--- infra/azure/terraform/variables.tf
+++ infra/azure/terraform/variables.tf
@@ -1,27 +1,32 @@
-# NOTE: We are using the recommended abbreviations for names from
+# NOTE: We are using the recommended abbreviations for default names from
 # the Microsoft Cloud Adoption Framework
 
 variable "acr_name" {
   description = "Name of the Azure Container Registry"
+  type        = string
   default     = "crhftd01"
 }
 
 variable "rg_name" {
   description = "Name of the resource group"
+  type        = string
   default     = "rg-we-hft-d-01"
 }
 
 variable "aks_name" {
   description = "Name of the Azure Kubernetes Service cluster"
+  type        = string
   default     = "aks-we-hft-d-01"
 }
 
 variable "location" {
   description = "Name of location where to deploy the resources"
+  type        = string
   default     = "West Europe"
 }
 
 variable "aks_node_pool_name" {
   description = "Name of the node pool to be used for the AKS cluster"
+  type        = string
   default     = "default"
 }
